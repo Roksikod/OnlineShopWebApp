c4749a35141deaaae231a07b0c775d4667bf8c87
Роксана Залесская <ksroksana@bk.ru>
2021-10-09 23:50:42 +0300

    ProductController was added

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -11,27 +11,33 @@ namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private List<Product> products = new List<Product>()
         {
-            _logger = logger;
-        }
-
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+            new Product ("Product1", 111, "Description1"),
+            new Product ("Product2", 222, "Description2"),
+            new Product ("Product3", 333, "Description3"),
+            new Product ("Product4", 444, "Description4"),
+        };
+        
+    public string Index()
+    {
+            var result = "";
+            foreach (var product in products)
+            {
+                result += product + "\n\n";
+            }
+        return result;
     }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
 }
---
+++ OnlineShop/OnlineShopWebApp/Controllers/Start.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class Start : Controller
+    {
+        public string Hello()
+        {
+            var currentHour = DateTime.Now.Hour;
+            if (currentHour < 6)
+            {
+                return "Good Night!";
+            }
+            if (currentHour < 12)
+            {
+                return "Good Morning!";
+            }            
+            if (currentHour < 18)
+            {
+                return "Good Afternoon!";
+            }
+            if (currentHour < 20)
+            {
+                return "Good Evening!";
+            }
+            return "Good Night!";
+            
+        }
+    }
+}
