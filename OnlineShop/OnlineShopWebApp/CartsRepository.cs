c7ee1e9882cd9b02acd843481d916e443137a825
Роксана Залесская <ksroksana@bk.ru>
2021-10-19 23:08:11 +0300

    Cart  was created

---
+++ OnlineShop/OnlineShopWebApp/CartsRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OnlineShopWebApp
+{
+    public static class CartsRepository
+    {
+        private static List<Cart> carts = new List<Cart>();
+
+        public static Cart TryGetByUserId(string userId)
+        {
+            return carts.FirstOrDefault(x=>x.UserId==userId);
+        }
+    }
+}
